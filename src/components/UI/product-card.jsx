97a06d5ba43321a42e0bc67eca28a0f95a51a9e8
Omar1030 <omarabdelmoaty816@gmail.com>
2025-11-03 20:13:56 +0200

    Fix the bug of title for producta card

--- src/components/UI/product-card.jsx
+++ src/components/UI/product-card.jsx
@@ -7,7 +7,7 @@ export default function ProductCard(props) {
           <img src={props.src} alt="product-img" loading="lazy" className="bg-gray-100  aspect-square object-fill w-full h-full hover:scale-[1.1] duration-300 cursor-pointer" />
         </figure>
       </Link>
-      <p className="card-title font-semibold ms-2 mt-2 whitespace-nowrap overflow-hidden text-ellipsis">{props.title}</p>
+      <p className="card-title  ms-2 mt-2 line-clamp-2 text-sm font-semibold min-h-[2.5rem] ">{props.title}</p>
       <ul className="flex">
         <li className="ms-2">
           <i className="fa-solid fa-star text-yellow-500"></i>
